cbd93e073341e0d27c146ff446a276b9ef498b25
muxintong <41592973+muxintong@users.noreply.github.com>
2022-06-13 11:44:47 +0800

    Update 167.两数之和 II - 输入有序数组two-sum-ii-input-array-is-sorted.py

--- 167.两数之和 II - 输入有序数组two-sum-ii-input-array-is-sorted.py
+++ 167.两数之和 II - 输入有序数组two-sum-ii-input-array-is-sorted.py
@@ -1,49 +1,47 @@
 """
-https://leetcode.com/problems/longest-palindromic-substring/submissions/
+https://labuladong.github.io/algo/2/18/23/
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 """
+from typing import List
+
 
 class Solution:
-    def palindrome(self, s: str, left: int, right: int):
-        # NOTE:边界条件
-        # 左边界需取到数组第一个元素0，右边界需取到数组最后一个元素len-1
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        res = s[left + 1:right]
-        return res
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
 
-    def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1: return s
+        while left < right:
+            val = numbers[left] + numbers[right]
+            if val == target:
+                break
+            elif val > target:
+                right -= 1
+            else:
+                left += 1
 
-        res = ""
-        for i in range(len(s)):
-            # 分奇odd、偶even讨论
-            odd = self.palindrome(s, i, i)
-            even = self.palindrome(s, i, i + 1)
-            if len(res) < len(odd): res = odd
-            if len(res) < len(even): res = even
-
-        return res
+        return [left + 1, right + 1]
 
 
 def main():
-    # Input: s = "babad"
-    # Output: "bab"
-    # Explanation: "aba" is also a valid answer.
-    soution1 = Solution()
-    print(soution1.longestPalindrome("babad"))
+    # Input: numbers = [2,7,11,15], target = 9
+    # Output: [1,2]
+    # Explanation: The sum of 2 and 7 is 9.
+    # Therefore, index1 = 1, index2 = 2. We return [1, 2].
+    soluiton1 = Solution()
+    print(soluiton1.twoSum([2,7,11,15], target = 9))
 
-    # Input: s = "cbbd"
-    # Output: "bb"
-    soution2 = Solution()
-    print(soution2.longestPalindrome("cbbd"))
+    # Input: numbers = [2,3,4], target = 6
+    # Output: [1,3]
+    # Explanation: The sum of 2 and 4 is 6.
+    # Therefore index1 = 1, index2 = 3. We return [1, 3].
+    soluiton1 = Solution()
+    print(soluiton1.twoSum(numbers = [2,3,4], target = 6))
 
-    # Input "a"
-    # Output ""
-    # Expected "a"
-    soution2 = Solution()
-    print(soution2.longestPalindrome("aa"))
-
+    # Input: numbers = [-1,0], target = -1
+    # Output: [1,2]
+    # Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+    soluiton1 = Solution()
+    print(soluiton1.twoSum(numbers = [-1,0], target = -1))
 
 if __name__ == '__main__':
     main()
