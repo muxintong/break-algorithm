96b08f6a1366d4cd4334f1cd3a248715c1587ddb
muxintong <41592973+muxintong@users.noreply.github.com>
2022-06-13 14:47:28 +0800

    Update 1143. Longest Common Subsequence.py

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -5,7 +5,7 @@ NOTE: 对于m行n列的二维数组
     python二维数组初始化：dp = [[0 for col in range(n+1)] for row in range(m+1)]    <=前列后行
 '''
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    def longestCommonSubsequence_dparr(self, text1: str, text2: str) -> int:
         m = len(text1)
         n = len(text2)
         # 定义：text1[0..i-1]和text2[0..j-1]的lcs长度为dp[i][j]
@@ -24,6 +24,32 @@ class Solution:
                     dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
         return dp[m][n]
+    
+    def longestCommonSubsequence_dpfunc(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        memo = [[0 for col in range(n)] for row in range(m)]
+
+        def dp(i: int, j: int) -> int:
+            # base case
+            if i == -1 or j == -1: return 0
+
+            # pre recursive：查memo
+            if memo[i][j] != 0: return memo[i][j]
+
+            # recursive
+            res = 0
+            if text1[i] == text2[j]:
+                res = dp(i - 1, j - 1) + 1
+            else:
+                res = max(dp(i - 1, j), dp(i, j - 1))
+
+            # post recursive: 写memo
+            memo[i][j] = res
+
+            return memo[i][j]
+
+        return dp(m - 1, n - 1)
 
 
 def main():
@@ -31,19 +57,19 @@ def main():
     # Output: 3
     # Explanation: The longest common subsequence is "ace" and its length is 3.
     solution1 = Solution()
-    print(solution1.longestCommonSubsequence(text1="abcde", text2="ace"))
+    print(solution1.longestCommonSubsequence_dparr(text1="abcde", text2="ace"))
 
     # Input: text1 = "abc", text2 = "abc"
     # Output: 3
     # Explanation: The longest common subsequence is "abc" and its length is 3.
     solution2 = Solution()
-    print(solution2.longestCommonSubsequence(text1="abc", text2="abc"))
+    print(solution2.longestCommonSubsequence_dparr(text1="abc", text2="abc"))
 
     # Input: text1 = "abc", text2 = "def"
     # Output: 0
     # Explanation: There is no such common subsequence, so the result is 0.
     solution3 = Solution()
-    print(solution3.longestCommonSubsequence(text1="abc", text2="def"))
+    print(solution3.longestCommonSubsequence_dparr(text1="abc", text2="def"))
 
 
 if __name__ == '__main__':
