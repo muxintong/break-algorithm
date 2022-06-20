aa583572bfb100009329284c9391ca44f95923c8
muxintong <41592973+muxintong@users.noreply.github.com>
2022-06-20 14:32:33 +0800

    Update 42.trapping-rain-water.py

--- double-pointer/left-right/[outer-to-center]/42.trapping-rain-water.py
+++ double-pointer/left-right/[outer-to-center]/42.trapping-rain-water.py
@@ -37,39 +37,6 @@ class Solution:
         return res
 
 
-    # 备忘录解法
-    def trap2(self, height: List[int]) -> int:
-        """
-        对于柱i，其所能容纳的最大雨水量为：
-        min(l_max[0-i], r_max[i,n]) - height[i]
-        """
-        n = len(height)
-        # 使用数组作为备忘录
-        l_max_memo = [0] * n
-        r_max_memo = [0] * n
-
-        # base case
-        l_max_memo[0] = height[0]
-        r_max_memo[n - 1] = height[n - 1]
-
-        # l_max：左至右
-        for i in range(1, n - 1, 1):
-            l_max_memo[i] = max(height[i], l_max_memo[i - 1])
-
-        # r_max：右至左
-        for i in range(n - 2, 0, -1):
-            r_max_memo[i] = max(height[i], r_max_memo[i + 1])
-
-        res = 0
-        for i in range(1, n - 1):
-            # NOTE:所接雨水量不能为负值，只有当其大于0时才将其计入答案中
-            temp = min(l_max_memo[i], r_max_memo[i]) - height[i]
-            if temp > 0:
-                res += temp
-
-        return res
-
-
 def main():
     # Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
     # Output: 6
