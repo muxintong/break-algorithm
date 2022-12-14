d354c297cc87d66b7b52e6b5777f6bc6403ecc05
muxintong <280999118@qq.com>
2022-12-14 16:48:08 +0800

    tree

--- dynamic planning/贪心/45.jump-game-ii.py
+++ dynamic planning/贪心/45.jump-game-ii.py
@@ -3,3 +3,53 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        memory = [n] * n
+
+        # dp定义：从索引p跳到最后一格，至少需要dp(nums,p)步
+        def dp(p: int) -> int:
+            if p >= n - 1: return 0
+
+            if memory[p] != n: return memory[p]
+
+            steps = nums[p]
+            for i in range(1, steps + 1):
+                subproblems = dp(p + i)
+                memory[p] = min(memory[p], subproblems + 1)
+
+            return memory[p]
+
+        return dp(0)
+
+    def jump_greedy(self, nums: List[int]) -> int:
+        n = len(nums)
+        # i、end标记了可以选择的跳跃步数
+        end = 0
+        # farthes记录最远的跳跃距离
+        farthest = 0
+        # jumps记录跳跃步数
+        jumps = 0
+        for i in range(n - 1):
+            farthest = max(farthest, nums[i] + i)
+            if end == i:
+                jumps += 1
+                end = farthest
+        return jumps
+
+
+def main():
+    # 输入: nums = [2,3,1,1,4]
+    # 输出: 2
+    # 解释: 跳到最后一个位置的最小跳跃数是 2。
+    #      从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+    solution1 = Solution()
+    print(solution1.jump_greedy(nums=[2, 3, 1, 1, 4]))
+
+    # 输入: nums = [2,3,0,1,4]
+    # 输出: 2
+    solution1 = Solution()
+    print(solution1.jump_greedy(nums=[2, 3, 0, 1, 4]))
+
+
+if __name__ == '__main__':
+    main()
--- dynamic planning/贪心/55.jump-game.py
+++ dynamic planning/贪心/55.jump-game.py
@@ -3,11 +3,9 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1: return True
-
         n = len(nums)
         farthest = 0
-        for i in range(n):
+        for i in range(n - 1):
             farthest = max(farthest, i + nums[i])
             if farthest <= i:
                 return False
@@ -16,29 +14,29 @@ class Solution:
 
 
 def main():
-    # # 输入：nums = [2,3,1,1,4]
-    # # 输出：true
-    # # 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
-    # solution1 = Solution()
-    # print(solution1.canJump(nums=[2, 3, 1, 1, 4]))
-    #
-    # # 输入：nums = [3,2,1,0,4]
-    # # 输出：false
-    # # 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
-    # solution1 = Solution()
-    # print(solution1.canJump(nums=[3, 2, 1, 0, 4]))
+    # 输入：nums = [2,3,1,1,4]
+    # 输出：true
+    # 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+    solution1 = Solution()
+    print(solution1.canJump(nums=[2, 3, 1, 1, 4]))
+
+    # 输入：nums = [3,2,1,0,4]
+    # 输出：false
+    # 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+    solution1 = Solution()
+    print(solution1.canJump(nums=[3, 2, 1, 0, 4]))
 
     # 输入：[0]
     # 输出：false
     # 预期结果：true
-    # solution1 = Solution()
-    # print(solution1.canJump(nums=[0]))
+    solution1 = Solution()
+    print(solution1.canJump(nums=[0]))
 
     # 输入：[2,0,0]
     # 输出：false
     # 预期结果：true
     solution1 = Solution()
-    print(solution1.canJump(nums=[2,0,0]))
+    print(solution1.canJump(nums=[2, 0, 0]))
 
 
 if __name__ == '__main__':
